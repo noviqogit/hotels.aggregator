ab107a52ff24eb24a2deff37d90bfa8a6be41ca8
sasha novikov <novikov.ae@gmail.com>
2023-05-09 21:25:08 +0300

    init db, env create models

--- aggregator/aggregator/settings.py
+++ aggregator/aggregator/settings.py
@@ -11,22 +11,21 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f8-sv!81^f!#0npb-ygtz-l$f4rx@s!i1l7y^i@0l-0g%721uh'
+# Initialise environment variables
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -69,17 +69,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'aggregator.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'admin',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -99,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -110,7 +110,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- aggregator/main/models.py
+++ aggregator/main/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Cities(models.Model):
+    city_id = models.IntegerField()
+    city_name = models.CharField()
+
+
+class Hotels(models.Model):
+    hotel_id = models.IntegerField()
+    hotel_name = models.CharField()
+    city_id = models.ForeignKey(Cities, on_delete=models.CASCADE)
+
+
+class Service(models.Model):
+    service_id = models.IntegerField()
+    service_name = models.CharField()
+    service_price = models.IntegerField()
+    hotel_id = models.ForeignKey(Hotels, on_delete=models.CASCADE)
+
+
+class Rooms(models.Model):
+    room_id = models.IntegerField()
+    room_name = models.CharField()
+    hotel_id = models.ForeignKey(Hotels, on_delete=models.CASCADE)
+
+
+class Devices(models.Model):
+    device_id = models.IntegerField()
+    room_id = models.ForeignKey(Rooms, null=True, on_delete=models.SET_NULL)
+
+
+class Clients(models.Model):
+    client_id = models.IntegerField()
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+    room_id = models.ForeignKey(Rooms, null=True, on_delete=models.SET_NULL)
+
+
+class Order(models.Model):
+    order_id = models.IntegerField()
+    client_id = models.ForeignKey(Clients, on_delete=models.CASCADE)
+    create_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+
+
+class OrderInfo(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    service_id = models.ForeignKey(Service, null=True, on_delete=models.SET_NULL)
